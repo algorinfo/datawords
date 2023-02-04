f15e113ce285555c856143007deeb597b91d168f
nuxion <nuxion@gmail.com>
2023-02-04 17:49:56 -0300

    new Word2Vec model interface

---
+++ datawords/models2.py
@@ -0,0 +1,176 @@
+import json
+from typing import Any, Iterable, List, Optional, Set
+
+import numpy as np
+from gensim.models import KeyedVectors, Word2Vec
+from gensim.models.phrases import ENGLISH_CONNECTOR_WORDS, FrozenPhrases, Phrases
+from pydantic import BaseModel
+
+from datawords import constants, parsers, utils
+
+
+class PhrasesModelMeta(BaseModel):
+    name: str
+    lang: str
+    parser_conf: parsers.ParserConf
+    min_count: int = 1
+    threshold: int = 1
+
+
+class PhrasesModel:
+    def __init__(
+        self,
+        parser_conf: parsers.ParserConf,
+        min_count=1,
+        threshold=1,
+        connector_words=None,
+        model=None,
+    ):
+
+        self._parser_conf = parser_conf
+        self._min_count = min_count
+        self._threshold = threshold
+        self._connector_words = (
+            connector_words or constants.CONNECTOR_WORDS[parser_conf.lang]
+        )
+        self._model = model
+        self._parser = self._parser_factory()
+
+    def _load_stopw(self):
+        return parsers.load_stop2(
+            self._parser_conf.lang, models_path=self._parser_conf.stopw_path
+        )
+
+    def _parser_factory(self) -> parsers.SentencesParser:
+        stopw = self._load_stopw()
+        return parsers.SentencesParser(
+            lang=self._parser_conf.lang,
+            lower=self._parser_conf.lower,
+            emo_codes=self._parser_conf.emo_codes,
+            strip_accents=self._parser_conf.strip_accents,
+            numbers=self._parser_conf.numbers,
+            stop_words=stopw,
+        )
+
+    def fit(self, X: Iterable):
+        sentences = parsers.SentencesIterator(X, parser=self._parser)
+        _model = Phrases(
+            sentences,
+            min_count=self._min_count,
+            threshold=1,
+            connector_words=self._connector_words,
+        )
+        self._model = _model.freeze()
+
+    def transform(self, X: Iterable):
+        results = []
+        for txt in X:
+            r = self._model[txt.split()]
+            results.append(r)
+        return results
+
+    def parse(self, txt: str):
+        r = self._model[txt.split()]
+        return r
+
+    def save(self, name: str, *, fp: str):
+        conf = PhrasesModelMeta(
+            name=name,
+            lang=self._parser_conf.lang,
+            parser_conf=self._parser_conf,
+            min_count=self._min_count,
+            threshold=self._threshold,
+        )
+        self._model.save(f"{fp}/{name}.bin")
+        with open(f"{fp}/{name}.json", "w") as f:
+            f.write(conf.json())
+
+    @classmethod
+    def load(cls, name, fp) -> "PhrasesModel":
+        with open(f"{fp}/{name}.json", "r") as f:
+            jmeta = json.loads(f.read())
+            meta = PhrasesModelMeta(**jmeta)
+        model = Phrases.load(f"{fp}/{name}.bin")
+        obj = cls(
+            parser_conf=meta.parser_conf,
+            min_count=meta.min_count,
+            threshold=meta.threshold,
+            model=model
+        )
+        return obj
+
+
+class Word2VecHelper:
+    def __init__(
+        self,
+        parser_conf: parsers.ParserConf,
+        phrases_model=None,
+        size=100,
+        window=5,
+        min_count=1,
+        workers=1,
+    ):
+
+        self._parser_conf = parser_conf
+        self._stopw = self._load_stopw(parser_conf.lang, self._parser_conf.stopw_path)
+        self._parser = self._parser_factory()
+        self._phrases = phrases_model
+        # self._models_path = models_path
+        self._min_count = min_count
+        self._size = size
+        self._window = window
+        self._min_count = min_count
+        self._workers = workers
+        self.wv = None
+
+    def _parser_factory(self) -> parsers.SentencesParser:
+        return parsers.SentencesParser(
+            lang=self._parser_conf.lang,
+            lower=self._parser_conf.lower,
+            phrases_model=self._phrases,
+            emo_codes=self._parser_conf.emo_codes,
+            strip_accents=self._parser_conf.strip_accents,
+            numbers=self._parser_conf.numbers,
+            stop_words=self._stopw,
+        )
+
+    def _load_stopw(self, lang, models_path):
+        return parsers.load_stop2(lang, models_path=models_path)
+
+    def fit(self, X: Iterable):
+        sentences = parsers.SentencesIterator(X, parser=self._parser)
+        model = Word2Vec(
+            sentences=sentences,
+            vector_size=self._size,
+            window=self._window,
+            min_count=self._min_count,
+            workers=self._workers,
+        )
+        self.wv = model
+
+    def parse(self, sentence: str):
+        return self._parser.parse(sentence)
+
+    def encode(self, sentence: str):
+        """
+        gets a sentence in plain text and encode it as vector
+        """
+        words = self.parse(sentence)
+        v = self.vectorize(words)
+        return v
+
+    def vectorize(self, sentence: List[str]):
+        """Get a vector from a list of words
+        if a sentence has words that don't match in the word2vec model,
+        then it fills with zeros
+        """
+        vectors = []
+        for word in sentence:
+            try:
+                _vect = self.wv[word]
+                vectors.append(_vect)
+            except KeyError:
+                pass
+        if len(vectors) > 0:
+            return np.sum(np.array(vectors), axis=0) / (len(vectors) + 0.001)
+        return np.zeros((self._size,))
